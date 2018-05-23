3d18fdd44da095efcb25c73369ee8f49697661d8
Nagababu Avula <avula.nagababu@gmail.com>
2018-05-23 11:23:06 -0400

    Create 3.Quotations.py

---
+++ 3.Quotations.py
@@ -0,0 +1,17 @@
+#Quotation in Python
+#Python accepts single ('), double (") and triple (''' or """) quotes to denote string literals, as long as the same type of quote starts and ends the string.
+
+#The triple quotes are used to span the string across multiple lines. For example, all the following are legal −
+
+word = 'OneWord '
+sentence = "This is a sentence. "
+paragraph = """This is a paragraph. 
+It has multiple lines and sentences. """
+
+print(word)
+print(sentence)
+print(paragraph)
+
+# or 
+print(word); print(sentence); print(paragraph)
+
