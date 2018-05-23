1888f7af8a6b005333ed9aadec17826bdf7404ed
Nagababu Avula <avula.nagababu@gmail.com>
2018-05-23 11:17:32 -0400

    Update 1. Hello_World.py

--- 1. Hello_World.py
+++ 1. Hello_World.py
@@ -1,1 +1,8 @@
 print ("Hello, World!")
+
+#A hash sign (#) that is not inside a string literal is the beginning of a comment. 
+#All characters after the #, up to the end of the physical line, are part of the comment 
+#and the Python interpreter ignores them.
+
+# Let us print 'Hello, World!'
+print ("Hello, World!")
