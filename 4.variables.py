e0d43f876d828dcf7af8b28dbdcbe743b2c6e832
Nagababu Avula <avula.nagababu@gmail.com>
2018-05-23 11:36:56 -0400

    Create 4.variables.py

---
+++ 4.variables.py
@@ -0,0 +1,11 @@
+num = 100          # An integer assignment
+deci   = 1000.0       # A floating point
+name    = "Naaga"       # A string
+
+print (num)
+print (deci)
+print (name)
+
+#Python allows you to assign a single value to several variables simultaneously.
+a = b = c = 1
+print (a+b+c)
