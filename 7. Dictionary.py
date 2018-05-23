d5e607016d3001caaff1d292dab185f06285cc71
Nagababu Avula <avula.nagababu@gmail.com>
2018-05-23 12:08:51 -0400

    Create 7. Dictionary.py

---
+++ 7. Dictionary.py
@@ -0,0 +1,16 @@
+# In Python dictionaries are like hash table data type
+# A hash table consists of key value pairs
+# Dictionaries are enclosed by curly braces { } and values can be assigned and accessed using square braces []
+
+dict = {}
+dict['abc'] = "This is abc"
+dict['xyz'] = "This is xyz"
+dict[9] = "number 9"
+
+dict2 = {'name': 'naaga','id':1234, 'dept': 'IT Ops'}
+
+print (dict['abc'])       # Prints value for 'abc' key
+print (dict[9])           # Prints value for 9 key
+print (dict2)             # Prints complete dictionary
+print (dict2.keys())   # Prints all the keys
+print (dict2.values()) # Prints all the values
