205427a451ff78f812d420102afbd2cafbc85ee9
Nagababu Avula <avula.nagababu@gmail.com>
2018-05-24 11:02:32 -0400

    Update 8.Type_cast.py

--- 8.Type_cast.py
+++ 8.Type_cast.py
@@ -1,1 +1,14 @@
+x = input()
+print('Data type of x is :'+ type(y)) #check data type
+# As we already mentioned by default input is a string
 
+y = input('Enter an integer : ')  #Example '234'
+print('Data type of y is :'+ type(y)) #check data type
+z = int(y)
+print('Data type of z is :'+ type(z)) #check data type
+
+
+y = input('Enter a float : ')  #Example '2.44'
+print('Data type of y is :'+ type(y)) #check data type
+z = float(y)
+print('Data type of z is :'+ type(z)) #check data type
