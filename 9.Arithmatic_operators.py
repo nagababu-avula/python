f446a556a771186ff15b206bde487bcaed000131
Nagababu Avula <avula.nagababu@gmail.com>
2018-05-24 17:07:28 -0400

    Update 9.Arithmatic_operators.py

--- 9.Arithmatic_operators.py
+++ 9.Arithmatic_operators.py
@@ -1,5 +1,5 @@
-a = int(input('Enter value for a': )
-b = int(input('Enter value for b': )
+a = int(input('Enter value for a': ))
+b = int(input('Enter value for b': ))
 print('a+b = ', a+b)
 print('a-b = ', a-b)
 print('a*b = ', a*b)
