2febcfc594a547e3edefca5de93c55d910fdbbc3
Nagababu Avula <avula.nagababu@gmail.com>
2018-05-24 15:15:58 -0400

    Update 10.conditions.py

--- 10.conditions.py
+++ 10.conditions.py
@@ -1,1 +1,14 @@
 
+a = int(input('Enter value for a': )
+b = int(input('Enter value for b': )
+
+if a==b:
+   print('a is equal to b.')
+        
+if a==b:
+   print('a is equal to b.')
+elif a>b:
+   print('a is greater than b.')   
+else:
+   print('a is less than b.')   
+        
