6da8934e82529284a6fcb7de8d88bab084176e4a
Nagababu Avula <avula.nagababu@gmail.com>
2018-05-24 15:13:08 -0400

    Create 9.Arithmatic_operators.py

---
+++ 9.Arithmatic_operators.py
@@ -0,0 +1,17 @@
+a = int(input('Enter value for a': )
+b = int(input('Enter value for b': )
+print('a+b = ', a+b)
+print('a-b = ', a-b)
+print('a*b = ', a*b)
+print('a/b = ', a/b)
+print('a%b = ', a%b)
+print('a**b = ', a**b)
+print('a//b = ', a//b)
+
+c = a+b
+print('c = ',c)
+c += 1
+print('c += 'c)
+
+
+
