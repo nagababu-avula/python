dc2df20b25b576f297c876337990095d2305f9ad
Nagababu Avula <avula.nagababu@gmail.com>
2018-05-25 16:48:43 -0400

    Create adv7.strings.py

---
+++ adv7.strings.py
@@ -0,0 +1,14 @@
+str = "I am a teacher"
+
+print("str: ",str)
+print ("str[0]: ", str[0])
+print ("str[1:5]: ", str[1:5])
+
+str2 = "Hi,"
+
+str3 = str2 + str
+print("concatenate :", str3)
+
+print(str*3)
+
+
