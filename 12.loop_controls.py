a07c942575a91d775bc4d49e9ea11c73a34c78d6
Nagababu Avula <avula.nagababu@gmail.com>
2018-05-25 15:24:40 -0400

    Create 12.loop_controls.py

---
+++ 12.loop_controls.py
@@ -0,0 +1,20 @@
+a = 20
+while a >= 0:
+  print(a)
+  if a%5 == 0:
+    break
+  a -= 5
+  
+  
+b = 10
+for i in range(b):
+  if b%2 == 0:
+    continue
+  print(b)
+  
+# Print odd numbers using a while loop and continue statement upto 20
+# Print all the numers upto 12 which is not devided by 3 using for lop and break statement.
+  
+  
+
+    
