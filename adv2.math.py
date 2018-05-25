aedc128437189efc216b72b4c47b180e09a4023c
Nagababu Avula <avula.nagababu@gmail.com>
2018-05-25 16:41:05 -0400

    Create adv2.math.py

---
+++ adv2.math.py
@@ -0,0 +1,27 @@
+a = 2.4
+b = -2.4
+
+print("a : ",a)
+print("b : ",b)
+
+print("abs(a) : ", abs(a))
+print("abs(b) : ", abs(b))
+
+print("ceil(a) : ", ceil(a))
+print("ceil(b) : ", ceil(b))
+
+print("ceil(a) : ", ceil(a))
+print("ceil(b) : ", ceil(b))
+
+print("exp(a) : ", exp(a)) # prints e**a
+
+print("fabs(a) : ", fabs(a))
+
+print("floor(a) : ", floor(a))
+
+print("log(a) : ", log(a))
+
+print("max(a,b) : ", max(a,b))
+
+print("random() :", random())
+
