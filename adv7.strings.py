fb3a8c102510785dbc9b0bf7652ada927fc783b6
Nagababu Avula <avula.nagababu@gmail.com>
2018-05-25 20:52:47 -0400

    Update adv7.strings.py

--- adv7.strings.py
+++ adv7.strings.py
@@ -1,8 +1,8 @@
 str = "I am a teacher"
 
 print("str: ",str)
-print ("str[0]: ", str[0])
-print ("str[1:5]: ", str[1:5])
+print ("str[0]: ", str[0]) # Characker
+print ("str[1:5]: ", str[1:5]) # Substring
 
 str2 = "Hi,"
 
@@ -10,5 +10,18 @@ str3 = str2 + str
 print("concatenate :", str3)
 
 print(str*3)
+
+print(in('t', str))
+
+print(capitalise(str))
+
+print(str.upper())
+
+print(str.lower())
+
+print(str.replace('am', 'was'))
+
+
+
 
 
