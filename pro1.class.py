39b4345ed7f920b82579819550a1b5b8892e9991
Nagababu Avula <avula.nagababu@gmail.com>
2018-05-26 18:45:45 -0400

    Create pro1.class.py

---
+++ pro1.class.py
@@ -0,0 +1,36 @@
+class Student:
+  count = 0
+  dept = ''
+  
+  def __init__(self, sid, sname):
+    self.id = sid
+    self.name = sname
+    Student.count += 1
+  
+  def disp(self):
+    print("id :", self.id, "Name :", self.name)
+    
+  def department(marks):
+    if marks > 80:
+      self.dept = "Computers"
+    elif marks > 60:
+      self.dept = "IT"
+    else
+      self.dept = "Software"
+      
+    return
+    
+s1 = Student(1001, 'Naaga')
+s2 = Student(1002, 'Chris')
+s3 = Student(1003, 'Sara')
+
+s1.disp()
+s2.disp()
+s3.department(78)
+
+print("Student Name and Department are : ", s3.name, s3.dept)
+
+print("Total no of students : ", Student.count)
+
+
+
