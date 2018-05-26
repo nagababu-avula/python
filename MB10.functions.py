df28cb2590f1197856151e554f8ea418b10e0006
Nagababu Avula <avula.nagababu@gmail.com>
2018-05-26 15:33:32 -0400

    Update MB10.functions.py

--- MB10.functions.py
+++ MB10.functions.py
@@ -1,40 +1,41 @@
 # Call a function to seperate a block of code for particular task / for repetitive task
 def sayhi():
   print("Hi, Everybody.")
+  return
 
 for i in range(10):
   sayhi()
 
 
-# Arguments passing by value
+# Arguments passing
 def add(x, y):
   z = x + y
   print("Addition : ", z)
+  return
   
 add(2, 3)
 
 
-# Arguments passing by reference
+# Arguments passing by variables
 def add(x, y):
   z = x + y
-  print("Addition : ", z)
+  return z
   
 a = 2
 b = 3
-add(a, b)
+c = add(a, b)
+print("Addition : ", c)
 
 
-# return a value from function
-def multiply(a,b):
-  x = a * b
+
+# Argument passing by reference #by default in python
+def modify(x):
+  x[1] = 3.87
   return x
   
-a = 2
-b = 3
-c = multiply(a, b)
-print("Multiplication : ", c)
-
-# Create a code for division by argument passing by reference
+lst = [2, 3, 4]
+modify(lst)
+print("Updated List : ", lst)
 
 
 
