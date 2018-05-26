60307d9c3d6d9bb791d055c03757e3e11d6c51f1
Nagababu Avula <avula.nagababu@gmail.com>
2018-05-25 21:13:31 -0400

    Update MB8.datatypes.py

--- MB8.datatypes.py
+++ MB8.datatypes.py
@@ -1,12 +1,20 @@
 
 # Updating Lists
-lst = [1, 3, 5, 'naaga', 7, 'Hi', 15.345]
+lst = [1, 3, 5, 'naaga',3, 'Hi', 15.345]
 print(lst)
-
-
-
 print(lst[2])
 print(lst[-2])
 print(length(lst)) #max, min
 
+lst.append('hello')
+print(lst)
+
+print(lst.count(3))
+
+lst.insert(2, 'hello')
+print(lst)
+
+print(lst.sort())
+
+print(lst.reverse())
 
