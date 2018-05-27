42a190b3bc51210c0992d81c15b286817561948e
Nagababu Avula <avula.nagababu@gmail.com>
2018-05-27 14:21:32 -0400

    Update Pro2.inheritance.py

--- Pro2.inheritance.py
+++ Pro2.inheritance.py
@@ -1,1 +1,22 @@
+class Student:
+  count = 0  # Class variable
+  
+  def __init__(self, sid, sname):
+    self.id = sid
+    self.name = sname
+  
+  def disp(self):
+    print("id :", self.id, "Name :", self.name)
+    
+    
+class ITstudent(Student):
+  def 
+  
+    
+s1 = Student(1001, 'Naaga')
+
+s2 = ITstudent(1002, 'Sara')
+
+
+
 
